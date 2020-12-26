84e0145923f50a609e58c2b32789010d6dc6e4c1
Julia Malinina <malinina.artwork@gmail.com>
2020-12-26 09:48:38 +0300

    add eslint packages

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,10 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-console */
+/* eslint-disable no-await-in-loop */
 import readlineSync from 'readline-sync';
 
- const letsPlay = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + name + '!');
+const letsPlay = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
 export default letsPlay;
